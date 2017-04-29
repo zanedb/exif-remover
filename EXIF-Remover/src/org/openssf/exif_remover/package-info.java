8fad4733529513cab64006aa4a7d05fa813a3189
Zane <zanedb@users.noreply.github.com>
2017-04-29 16:16:06 -0700

    Delete package-info.java

--- EXIF-Remover/src/org/openssf/exif_remover/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Open Software Services Foundation (@openssf on GitHub)
- * @license GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
- * @version 1.0
- */
-package org.openssf.exif_remover;
